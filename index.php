3551def8e54edab33d587627d92d309d4c1b9c2e
Shubham <shubhamdubey57@gmail.com>
2023-12-27 03:03:58 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @package    WordPress
+ * @subpackage Twenty_Twenty_One
+ * @link       https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * @since      Twenty Twenty-One 1.0
+ */
+
+get_header(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress-theme</title>
+</head>
+<body>
+    <h1>Wordpress-theme</h1>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,12 @@
+# FILEPATH
+
+"""
+WordPress Theme Template Readme File
+
+This file serves as the readme documentation for the WordPress theme template. It provides an overview of the theme, installation instructions, and other important information.
+
+Author: [Your Name]
+Version: 1.0.0
+"""
+
+# Rest of the code...
